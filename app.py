a3e1521aafb59c846a86fcb83d69e07c291463fb
filipe11402 <filipe11402@outlook.pt>
2021-04-26 11:01:11 +0100

    classmethod to search for account

--- app.py
+++ app.py
@@ -19,3 +19,12 @@ class Account(Base):
     def __str__(self):
         return f'Username: {self.firstname} {self.lastname}\nAccount balance: {self.balance}'
 
+    @classmethod
+    def search_account(self, firstname, lastname):
+        for row in session.query(Account.firstname, Account.lastname):
+            if firstname in row and lastname in row:
+                print("ja existe uma conta com esse nome")
+            else:
+                print("False")
+
+Account.search_account('Teste', 'do teste')
